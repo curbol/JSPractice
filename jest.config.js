9e695fd26458cdb901e9ed2a1b45c69c8340068a
Curtis Bollinger <curtis.bollinger@gmail.com>
2020-04-14 08:37:48 -0500

    fixed file import paths

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-  },
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
   },
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/scripts/**/*.js', '<rootDir>/scripts/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/scripts/**/*.js',
+    '<rootDir>/scripts/**/*.ts',
+  ],
 }
---
+++ test/add.test.ts
@@ -0,0 +1,5 @@
+import { add } from '../scripts/add'
+
+it('adds 2 + 1 to equal 3', () => {
+  expect(add(2, 1)).toBe(3)
+})
